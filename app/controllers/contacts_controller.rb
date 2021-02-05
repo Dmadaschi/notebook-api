651602bc53d68f9aee011bee89b59535ec449569
Dmadaschi <nunean@gmail.com>
2021-02-04 21:47:34 -0300

    create model kind

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,19 +1,16 @@
 class ContactsController < ApplicationController
-  before_action :set_contact, only: [:show, :update, :destroy]
+  before_action :set_contact, only: %i[show update destroy]
 
-  # GET /contacts
   def index
     @contacts = Contact.all
 
     render json: @contacts
   end
 
-  # GET /contacts/1
   def show
     render json: @contact
   end
 
-  # POST /contacts
   def create
     @contact = Contact.new(contact_params)
 
@@ -24,7 +21,6 @@ class ContactsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /contacts/1
   def update
     if @contact.update(contact_params)
       render json: @contact
@@ -33,19 +29,17 @@ class ContactsController < ApplicationController
     end
   end
 
-  # DELETE /contacts/1
   def destroy
     @contact.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_contact
-      @contact = Contact.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def contact_params
-      params.require(:contact).permit(:name, :email, :birthdate)
-    end
+  def set_contact
+    @contact = Contact.find(params[:id])
+  end
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :birthdate, :kind_id)
+  end
 end
--- lib/tasks/dev.rake
+++ lib/tasks/dev.rake
@@ -1,16 +1,21 @@
 namespace :dev do
-  desc "Configura o ambiente de desenvolvimento"
+  desc 'Configura o ambiente de desenvolvimento'
   task setup: :environment do
-    puts "Cadastrando contatos"
+    puts 'Cadastrando tipós de contato'
+    %w[Amigo Comercial Conhecido].each do |kind|
+      Kind.create!(description: kind)
+    end
+    puts 'Tipos de Contato Cadastrados com sucesso'
 
+    puts 'Cadastrando contatos'
     100.times do |i|
       Contact.create!(
         name: Faker::Name.name,
         email: Faker::Internet.email,
-        birthdate: Faker::Date.between(from: 65.years.ago, to: 18.years.ago)
+        birthdate: Faker::Date.between(from: 65.years.ago, to: 18.years.ago),
+        kind: Kind.all.sample
       )
     end
-
-    puts "Contatos Cadastrados com sucesso"
+    puts 'Contatos Cadastrados com sucesso'
   end
 end
