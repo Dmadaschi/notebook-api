b7d4b27c541de1352ff14594fc14bc530ee273dc
Dmadaschi <nunean@gmail.com>
2021-02-08 17:08:50 -0300

    adD HATEOS to contact

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,9 +1,19 @@
 class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate
 
-  belongs_to :kind
+  belongs_to :kind do
+    link(:kind) { kind_url(object.kind.id) }
+  end
   has_one :address
   has_many :phones
+
+  link(:self) { contact_url(object.id) }
+
+  meta do
+    {
+      author: 'Daniel Madaschi'
+    }
+  end
 
   def attributes(*args)
     h = super(*args)
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,7 +42,6 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
@@ -51,4 +50,9 @@ Rails.application.configure do
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.file_watcher = ActiveSupport::FileUpdateChecker
+
+  Rails.application.routes.default_url_options = {
+    host: 'localhost',
+    port: 3000
+  }
 end
